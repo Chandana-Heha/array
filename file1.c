02f3f024f0ec9d85a7cfdb18506914e56f8bba4d
Chandana-Heha <gokarajuchandana861@gmail.com>
2026-08-17 11:36:53 +0530

    quesion 1

---
+++ file1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n;
+    int largest, secondLargest;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (arr[0] > arr[1]) {
+        largest = arr[0];
+        secondLargest = arr[1];
+    } else {
+        largest = arr[1];
+        secondLargest = arr[0];
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("Second largest number = %d", secondLargest);
+
+    return 0;
+}
